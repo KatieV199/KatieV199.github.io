6b97f0b962bdb48766c8df510af7795101137138
Ekaterina <31217426+KatieV199@users.noreply.github.com>
2018-11-22 17:05:11 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function() {
+    jQuery(".button-down").click(function() {
+        jQuery("ul.menu").slideToggle(300);
+
+    });
+
+    jQuery("ul.menu span").click(function() {
+        jQuery("ul.menu").slideToggle(300);
+    });
+
+
+    jQuery(".category-in").click(function() {
+        jQuery(".main-left-side-list").toggleClass('active');
+
+    });
+
+    jQuery("#add-btn").click(function() {
+        jQuery(".add-form").slideToggle(300);
+
+    });
+
+});
